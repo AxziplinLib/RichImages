f5b0dbe4e35e013e6634ff5d7532e0c2993ca3ee
aixing-hb <aixing@hydsoft.com>
2017-09-04 10:12:57 +0800

    Initial Commit

---
+++ RichImages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RichImages
+//
+//  Created by devedbox on 2017/9/4.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
