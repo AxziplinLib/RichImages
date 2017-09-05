fbbdf5c0a6cfef4317d502634c4da254ebddb5c9
aixing-hb <aixing@hydsoft.com>
2017-09-05 14:54:18 +0800

    Fixed the code style.

--- Source/Filters.swift
+++ Source/Filters.swift
@@ -16,18 +16,18 @@ fileprivate struct _Kernel {}
 extension _Kernel {
     /// Returns the kernel codes to generate a rounded radius effect.
     fileprivate static var roundRadius: String {
-        return "kernel vec4 roundr(sampler src, float r)"   +
-        "{"                                                 +
-            "vec2  p = destCoord();"                        +
-            "vec4  extent = samplerExtent(src);"            +
-            "vec4  pixel = sample(src, samplerCoord(src));" +
-            "float ty = extent.w - r;"                      +
-            "float rx = extent.z - r;"                      +
-            "float r_s = pow(r, 2.0);"                      +
-            "float xd_s = pow((p.x - r), 2.0);"             +
-            "float yd_s = pow((p.y - r), 2.0);"             +
-            "float rxd_s = pow((p.x - rx), 2.0);"           +
-            "float tyd_s = pow((p.y - ty), 2.0);"           +
+        return "kernel vec4 roundr(sampler src, float r)"    +
+        "{"                                                  +
+            "vec2  p      = destCoord();"                    +
+            "vec4  extent = samplerExtent(src);"             +
+            "vec4  pixel  = sample(src, samplerCoord(src));" +
+            "float ty     = extent.w - r;"                   +
+            "float rx     = extent.z - r;"                   +
+            "float r_s    = pow(r, 2.0);"                    +
+            "float xd_s   = pow((p.x - r), 2.0);"            +
+            "float yd_s   = pow((p.y - r), 2.0);"            +
+            "float rxd_s  = pow((p.x - rx), 2.0);"           +
+            "float tyd_s  = pow((p.y - ty), 2.0);"           +
             "if ((xd_s + yd_s) >= r_s && (xd_s + tyd_s) >= r_s && (rxd_s + yd_s) >= r_s && (rxd_s + tyd_s) >= r_s) {" +
                 "if (p.x <= r  && p.y <= r ) { pixel.a = max(0.0, 1.0 - (sqrt((xd_s  + yd_s)) - r ) * 1.0); }"        +
                 "if (p.x <= r  && p.y >= ty) { pixel.a = max(0.0, 1.0 - (sqrt((xd_s  + tyd_s)) - r) * 1.0); }"        +
