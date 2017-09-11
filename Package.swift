7b371201196f6bf662db58c3cbc89d05d8ba3308
aixing-hb <aixing@hydsoft.com>
2017-09-11 09:11:34 +0800

    Added Package.swift.

---
+++ Package.swift
@@ -0,0 +1,11 @@
+//
+//  Package.swift
+//  RichImages
+//
+//  Created by devedbox on 2017/9/11.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(name: "RichImages", dependencies : [], exclude: [])
