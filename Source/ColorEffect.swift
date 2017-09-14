b46829cac13a35c0de1e2fb385498b0a84314abd
aixing-hb <aixing@hydsoft.com>
2017-09-14 17:48:25 +0800

    Added filter `CIColorCrossPolynomial`.

--- Source/ColorEffect.swift
+++ Source/ColorEffect.swift
@@ -70,4 +70,35 @@ extension CIVector {
 public protocol ColorEffectAppliable: RichImagable { }
 
 extension ColorEffectAppliable {
+    /// Modifies the pixel values in an image by applying a set of polynomial cross-products.
+    ///
+    /// Each component in an output pixel out is determined using the component values in the input pixel in according
+    /// to a polynomial cross product with the input coefficients. That is, the red component of the output pixel is 
+    /// calculated using the inputRedCoefficients parameter (abbreviated rC below) using the following formula:
+    ///```
+    /// out.r =        in.r * rC[0] +        in.g * rC[1] +        in.b * rC[2]
+    /// + in.r * in.r * rC[3] + in.g * in.g * rC[4] + in.b * in.b * rC[5]
+    /// + in.r * in.g * rC[6] + in.g * in.b * rC[7] + in.b * in.r * rC[8]
+    /// + rC[9]
+    ///```
+    /// Then, the formula is repeated to calculate the blue and green components of the output pixel using the blue and 
+    /// green coefficients, respectively.
+    ///
+    /// This function can be used for advanced color space and tone mapping conversions, such as imitating the color reproduction
+    /// of vintage photography film.
+    ///
+    /// - Parameter red   : The `red` field polynomial coefficients to modify the color source image. Using [1.0, 0.0, ...] as default.
+    /// - Parameter green : The `green` field polynomial coefficients to modify the color source image. Using [0.0, 1.0, 0.0, ...] as default.
+    /// - Parameter blue  : The `blue` field polynomial coefficients to modify the color source image. Using [0.0, 0.0, 1.0, 0.0, ...] as default.
+    /// - Parameter option: A value of `RichImage.RenderOption` indicates the rendering options of the image processing.
+    ///                     Note that the CPU-Based option is not available in ths section. Using `.auto` by default.
+    ///
+    /// - Returns: A copy of the receiver modified with the given cross polynomial coefficients.
+    public func crossPolynomial(red: RichImage.ColorCrossPolynomial = [1.0], green: RichImage.ColorCrossPolynomial = [0.0, 1.0], blue: RichImage.ColorCrossPolynomial = [0.0, 0.0, 1.0], option: RichImage.RenderOption = .auto) -> UIImage! {
+        guard let ciImage = image._makeCiImage()?.applyingFilter("CIColorCrossPolynomial", withInputParameters: ["inputRedCoefficients": CIVector(colorCrossPolynomial: red), "inputGreenCoefficients": CIVector(colorCrossPolynomial: green), "inputBlueCoefficients": CIVector(colorCrossPolynomial: blue)]),
+              let _image = type(of: self).make(ciImage, from: CGRect(origin: .zero, size: image.size.scale(by: image.scale)), scale: image.scale, orientation: image.imageOrientation, option: option) else {
+                return nil
+        }
+        return _image
+    }
 }
