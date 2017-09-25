d5b93780f2148335e0e46b56bd39a8e7a64e425a
aixing-hb <aixing@hydsoft.com>
2017-09-25 16:55:22 +0800

    Added filter CIPhotoEffectMono.

--- Source/ColorEffect.swift
+++ Source/ColorEffect.swift
@@ -328,6 +328,18 @@ extension ColorEffectAppliable {
         }
         return img
     }
+    /// Applies a preconfigured set of effects that imitate black-and-white photography film with low contrast.
+    ///
+    /// - Parameter option: A value of `RichImage.RenderOption` indicates the rendering options of the image processing.
+    ///                     Note that the CPU-Based option is not available in ths section. Using `.auto` by default.
+    /// - Returns: A copy of the source image by applying photo effect mono.
+    public func mono(option: RichImage.RenderOption = .auto) -> UIImage! {
+        guard let ciImage = image._makeCiImage()?.applyingFilter("CIPhotoEffectMono", withInputParameters: nil),
+              let img = type(of: self).make(ciImage, from: CGRect(origin: .zero, size: image.size.scale(by: image.scale)), scale: image.scale, orientation: image.imageOrientation, option: option) else {
+                return nil
+        }
+        return img
+    }
 }
 
 /// ColorEffectAppliable conformance of UIImage.
