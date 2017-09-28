ca79f3d563b1b3b843da570670573662a2801835
aixing-hb <aixing@hydsoft.com>
2017-09-28 11:20:46 +0800

    Added *Framework target to support carthage.

---
+++ RichImagesFramework/RichImagesFramework.h
@@ -0,0 +1,19 @@
+//
+//  RichImagesFramework.h
+//  RichImagesFramework
+//
+//  Created by devedbox on 2017/9/28.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RichImagesFramework.
+FOUNDATION_EXPORT double RichImagesFrameworkVersionNumber;
+
+//! Project version string for RichImagesFramework.
+FOUNDATION_EXPORT const unsigned char RichImagesFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RichImagesFramework/PublicHeader.h>
+
+
