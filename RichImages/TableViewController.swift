a5abf880fdd2b9097cc8d3bed29069d1c3a2003b
devedbox <862099730@qq.com>
2017-11-30 16:19:28 +0800

    Added sample codes.

---
+++ RichImages/TableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TableViewController.swift
+//  RichImages
+//
+//  Created by devedbox on 2017/11/30.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        if #available(iOS 11.0, *) {
+            tableView.contentInsetAdjustmentBehavior = .never
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: Table view delegate.
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
