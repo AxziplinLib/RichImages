1e05f487c042c299f7f9bcb76a993536fc2df37a
devedbox <862099730@qq.com>
2017-12-08 23:57:43 +0800

    Added type: ._SizeFitsImageView.

--- Source/View Controller Supportings/ImageEditingViewController.swift
+++ Source/View Controller Supportings/ImageEditingViewController.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
-private func _createContainerScrollView() -> UIScrollView {
-    let scrollView = UIScrollView(frame: .zero)
-    scrollView.translatesAutoresizingMaskIntoConstraints = false
-    scrollView.backgroundColor = .clear
-    return scrollView
-}
-
 class ImageEditingViewController: UIViewController {
     /// The scroll view of the visible content.
-    private lazy var _scrollView: UIScrollView = _createContainerScrollView()
+    private lazy var _scrollView: UIScrollView = { () -> UIScrollView in
+        let scrollView = UIScrollView(frame: .zero)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .clear
+        return scrollView
+    }()
+    /// Content view for image, tool bar or other views.
+    private lazy var _contentView: UIView = { () -> UIView in
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    /// Image view to show the result of image.
+    private lazy var _imageView: _SizeFitsImageView = { () -> _SizeFitsImageView in
+        let imageView = _SizeFitsImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,11 +55,23 @@ class ImageEditingViewController: UIViewController {
 
 }
 
-// MARK: Public Interface.
+// MARK: - _SizeFitsImageView.
+
+extension ImageEditingViewController {
+    fileprivate class _SizeFitsImageView: UIImageView {
+        override var intrinsicContentSize: CGSize {
+            return super.intrinsicContentSize
+        }
+    }
+}
+
+// MARK: - Public Interface.
 
 extension ImageEditingViewController {
     public var scrollView: UIScrollView { return _scrollView }
 }
+
+// MARK: - Private.
 
 extension ImageEditingViewController {
     /// Add scroll view to the root view and add constraints to the scroll view.
@@ -59,5 +81,13 @@ extension ImageEditingViewController {
         _scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         _scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         _scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    fileprivate func _setupContentView() {
+        _scrollView.addSubview(_contentView)
+        _contentView.leadingAnchor.constraint(equalTo: _scrollView.leadingAnchor).isActive = true
+        _contentView.trailingAnchor.constraint(equalTo: _scrollView.trailingAnchor).isActive = true
+        _contentView.topAnchor.constraint(equalTo: _scrollView.topAnchor).isActive = true
+        _contentView.bottomAnchor.constraint(equalTo: _scrollView.bottomAnchor).isActive = true
     }
 }
